62e465c9d72c4d031ed1a0835c90a58cfc4f5959
mohammedgqudah <mohghq@gmail.com>
2026-06-20 19:12:07 +0300

    init

---
+++ src/main.zig
@@ -0,0 +1,101 @@
+const std = @import("std");
+const stdx = @import("stdx.zig");
+
+const Io = std.Io;
+
+const zig_simple_kv = @import("zig_simple_kv");
+
+pub fn main(init: std.process.Init) !void {
+    const cwd = std.Io.Dir.cwd();
+    const db = try blk: {
+        if (cwd.openFile(init.io, "db.z", .{ .mode = .read_write })) |f| {
+            break :blk f;
+        } else |err| {
+            if (err == std.Io.Dir.OpenError.FileNotFound) {
+                break :blk createDatabase(init.io, cwd);
+            } else {
+                break :blk err;
+            }
+        }
+    };
+
+    _ = try setKey(db, "abc", "123");
+}
+
+pub fn createDatabase(io: std.Io, cwd: std.Io.Dir) !std.Io.File {
+    const file = try cwd.createFile(io, "db.z", .{
+        .truncate = false,
+        .read = true,
+    });
+
+    var super = SuperBlock{
+        .free_offset = @sizeOf(SuperBlock),
+    };
+    super.nativeToEndian();
+
+    try file.writePositionalAll(io, std.mem.asBytes(&super), 0);
+
+    return file;
+}
+
+const SuperBlock = extern struct {
+    const Magic = 0xdeadbeefcafebabe;
+
+    magic: u64 = Magic,
+    count: u64 = 0, // total number of keys
+    free_offset: u64 = 0, // offset for next free spot
+
+    pub fn endianToNative(self: *SuperBlock) void {
+        const info = @typeInfo(SuperBlock);
+        inline for (info.@"struct".fields) |field| {
+            switch (@typeInfo(field.type)) {
+                .int => {
+                    @field(self, field.name) = std.mem.littleToNative(field.type, @field(self, field.name));
+                },
+                .float => @compileError("floats are not supported in SuperBlock"),
+                else => {},
+            }
+        }
+    }
+
+    pub fn nativeToEndian(self: *SuperBlock) void {
+        const info = @typeInfo(SuperBlock);
+        inline for (info.@"struct".fields) |field| {
+            switch (@typeInfo(field.type)) {
+                .int => {
+                    @field(self, field.name) = std.mem.nativeToLittle(field.type, @field(self, field.name));
+                },
+                .float => @compileError("floats are not supported in SuperBlock"),
+                else => {},
+            }
+        }
+    }
+};
+
+pub fn loadSuperBlock(db: std.Io.File) !SuperBlock {
+    var super: SuperBlock = undefined;
+    _ = try stdx.pread(db.handle, std.mem.asBytes(&super), 0);
+    super.endianToNative();
+
+    if (super.magic != SuperBlock.Magic) {
+        return error.InvalidMagicNumber;
+    }
+
+    return super;
+}
+
+pub fn setKey(db: std.Io.File, key: []const u8, value: []const u8) !void {
+    const super = try loadSuperBlock(db);
+
+    const key_len: usize = key.len;
+    const val_len: usize = value.len;
+    // write key and value in Pascal format
+    const iovec: []const std.posix.iovec_const = &.{
+        .{ .base = @ptrCast(&key_len), .len = @sizeOf(usize) },
+        .{ .base = key.ptr, .len = key_len },
+
+        .{ .base = @ptrCast(&val_len), .len = @sizeOf(usize) },
+        .{ .base = value.ptr, .len = val_len },
+    };
+    _ = try stdx.pwritev(db.handle, &iovec, super.free_offset);
+}
---
+++ src/stdx.zig
@@ -0,0 +1,79 @@
+const std = @import("std");
+
+pub const fd_t = std.posix.system.fd_t;
+const ReadError = std.posix.ReadError;
+const native_os = std.posix.ReadError;
+const errno = std.posix.errno;
+const unexpectedErrno = std.posix.unexpectedErrno;
+
+pub fn pread(fd: fd_t, buf: []u8, offset: usize) ReadError!usize {
+    if (buf.len == 0) return 0;
+
+    // Prevents EINVAL.
+    const max_count = 0x7ffff000;
+
+    while (true) {
+        const rc = std.os.linux.pread(fd, buf.ptr, @min(buf.len, max_count), @intCast(offset));
+        switch (errno(rc)) {
+            .SUCCESS => return @intCast(rc),
+            .INTR => continue,
+            .INVAL => unreachable,
+            .FAULT => unreachable,
+            .AGAIN => return error.WouldBlock,
+            .CANCELED => return error.Canceled,
+            .BADF => return error.Unexpected,
+            .IO => return error.InputOutput,
+            .ISDIR => return error.IsDir,
+            .NOBUFS => return error.SystemResources,
+            .NOMEM => return error.SystemResources,
+            .NOTCONN => return error.SocketUnconnected,
+            .CONNRESET => return error.ConnectionResetByPeer,
+            .TIMEDOUT => return error.Unexpected,
+            else => |err| return unexpectedErrno(err),
+        }
+    }
+}
+
+pub fn pwrite(fd: fd_t, buf: []u8, offset: usize) ReadError!usize {
+    if (buf.len == 0) return 0;
+
+    while (true) {
+        const rc = std.os.linux.pread(fd, buf.ptr, buf.len, @intCast(offset));
+        switch (errno(rc)) {
+            .SUCCESS => return @intCast(rc),
+            .INTR => continue,
+            .INVAL => error.InvalidValue,
+            .FAULT => unreachable,
+            .AGAIN => return error.WouldBlock,
+            .CANCELED => return error.Canceled,
+            .BADF => return error.Unexpected,
+            .IO => return error.InputOutput,
+            .ISDIR => return error.IsDir,
+            .NOBUFS => return error.SystemResources,
+            .NOMEM => return error.SystemResources,
+            .TIMEDOUT => return error.Unexpected,
+            else => |err| return unexpectedErrno(err),
+        }
+    }
+}
+
+pub fn pwritev(fd: fd_t, iovec: *const []const std.posix.iovec_const, offset: usize) ReadError!usize {
+    while (true) {
+        const rc = std.os.linux.pwritev(fd, iovec.ptr, iovec.len, @intCast(offset));
+        switch (errno(rc)) {
+            .SUCCESS => return @intCast(rc),
+            .INTR => continue,
+            .INVAL => unreachable,
+            .FAULT => unreachable,
+            .AGAIN => return error.WouldBlock,
+            .CANCELED => return error.Canceled,
+            .BADF => return error.Unexpected,
+            .IO => return error.InputOutput,
+            .ISDIR => return error.IsDir,
+            .NOBUFS => return error.SystemResources,
+            .NOMEM => return error.SystemResources,
+            .TIMEDOUT => return error.Unexpected,
+            else => |err| return unexpectedErrno(err),
+        }
+    }
+}
