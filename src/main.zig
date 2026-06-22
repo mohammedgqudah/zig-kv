12ab548256d0268efeefec2a3621654778a64e69
mohammedgqudah <mohghq@gmail.com>
2026-06-22 19:10:05 +0300

    use a Session object as context

--- src/main.zig
+++ src/main.zig
@@ -7,34 +7,51 @@ const mem = std.mem;
 
 const zig_simple_kv = @import("zig_simple_kv");
 
+/// a Session represents an open database. This will be used
+/// as a context object throughout the code.
+const Session = struct {
+    io: std.Io,
+    gpa: std.mem.Allocator,
+    db_file: std.Io.File,
+
+    /// TODO: Never directly modify this object
+    /// but for now make sure you call writeSuperBlock after modifying it
+    super: SuperBlock,
+
+    pub fn open(io: std.Io, gpa: std.mem.Allocator, cwd: std.Io.Dir, path: []const u8) !Session {
+        const file = cwd.openFile(io, path, .{ .mode = .read_write }) catch |err| switch (err) {
+            error.FileNotFound => try createDatabase(io, cwd, path),
+            else => return err,
+        };
+
+        return .{
+            .gpa = gpa,
+            .io = io,
+            .db_file = file,
+            .super = try loadSuperBlock(file),
+        };
+    }
+};
+
 pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
     const cwd = std.Io.Dir.cwd();
-    const db = try blk: {
-        if (cwd.openFile(init.io, "db.z", .{ .mode = .read_write })) |f| {
-            break :blk f;
-        } else |err| {
-            if (err == std.Io.Dir.OpenError.FileNotFound) {
-                break :blk createDatabase(init.io, cwd);
-            } else {
-                break :blk err;
-            }
-        }
-    };
 
-    _ = try setKey(db, "abc", "123");
+    var session = try Session.open(init.io, gpa, cwd, "db.z");
+    _ = try setKey(&session, "abc", "123");
     //_ = try setKey(db, "abc", "555");
     //_ = try setKey(db, "hello", "AAA");
 
-    const val = try getKey(gpa, db, "abc") orelse unreachable;
+    const val = try getKey(&session, "abc") orelse unreachable;
     defer gpa.free(val);
 
     assert(mem.eql(u8, val, "123"));
     //assert(mem.eql(u8, try getKey(gpa, db, "hello") orelse unreachable, "AAA"));
 }
 
-pub fn createDatabase(io: std.Io, cwd: std.Io.Dir) !std.Io.File {
-    const file = try cwd.createFile(io, "db.z", .{
+/// Creates the database file and initializes the super block.
+pub fn createDatabase(io: std.Io, cwd: std.Io.Dir, path: []const u8) !std.Io.File {
+    const file = try cwd.createFile(io, path, .{
         .truncate = false,
         .read = true,
     });
@@ -109,8 +126,8 @@ pub fn writeSuperBlock(db: std.Io.File, super: *const SuperBlock) !void {
     _ = try stdx.pwritev(db.handle, &iovec, 0);
 }
 
-pub fn setKey(db: std.Io.File, key: []const u8, value: []const u8) !void {
-    var super = try loadSuperBlock(db);
+pub fn setKey(session: *Session, key: []const u8, value: []const u8) !void {
+    var super = session.super;
 
     const key_len: usize = key.len;
     const val_len: usize = value.len;
@@ -124,20 +141,17 @@ pub fn setKey(db: std.Io.File, key: []const u8, value: []const u8) !void {
         .{ .base = key.ptr, .len = key_len },
         .{ .base = value.ptr, .len = val_len },
     };
-    _ = try stdx.pwritev(db.handle, &iovec, super.free_offset);
+    _ = try stdx.pwritev(session.db_file.handle, &iovec, super.free_offset);
 
     super.free_offset += key_len + val_len + (@sizeOf(usize) * 2);
-    try writeSuperBlock(db, &super);
+    try writeSuperBlock(session.db_file, &super);
 }
 
-pub fn getKey(allocator: std.mem.Allocator, db: std.Io.File, key: []const u8) !?[]const u8 {
-    const super = try loadSuperBlock(db);
-    _ = super;
-
+pub fn getKey(session: *const Session, key: []const u8) !?[]const u8 {
     var offset: usize = @sizeOf(SuperBlock);
     var buffer: [0x1000]u8 = undefined;
     while (true) {
-        if (try stdx.pread(db.handle, &buffer, offset) == 0) {
+        if (try stdx.pread(session.db_file.handle, &buffer, offset) == 0) {
             break;
         }
 
@@ -149,7 +163,7 @@ pub fn getKey(allocator: std.mem.Allocator, db: std.Io.File, key: []const u8) !?
         if (std.mem.eql(u8, cand_key, key)) {
             const cand_value = buffer[value_start .. value_start + value_size];
             std.log.info("found", .{});
-            const ret = try allocator.alloc(u8, cand_value.len);
+            const ret = try session.gpa.alloc(u8, cand_value.len);
             @memcpy(ret, cand_value);
             return ret;
         }
