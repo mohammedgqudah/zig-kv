5da3233659891779ed40e3825638f3f594b01cc0
mohammedgqudah <mohghq@gmail.com>
2026-09-12 16:32:47 +0300

    checkpoint: split page, wip parent pointers

--- src/page.zig
+++ src/page.zig
@@ -142,7 +142,7 @@ pub const PageBuffer = struct {
         };
     }
 
-    pub fn header(self: *Self) *PageHeader {
+    pub inline fn header(self: *Self) *PageHeader {
         return @ptrCast(@alignCast(self.inner.ptr));
     }
 
@@ -192,6 +192,13 @@ pub const FindResult = struct {
     /// Index of the pointer that points to key upper bound in page.
     /// If null, then the key is the greatest in the page.
     upper_bound_idx: ?usize,
+    /// If "track_path" was set. The caller owns this allocation
+    path: ?std.ArrayList(PageId) = null,
+};
+
+pub const FindOptions = struct {
+    /// Track pages while traversing the tree
+    track_path: bool,
 };
 
 pub const Tree = struct {
@@ -200,6 +207,7 @@ pub const Tree = struct {
     file: Io.File,
     root_page_id: u64 = 0,
     page_cache: PageCache,
+    allocator: mem.Allocator,
 
     /// initialize a new tree, `file` is expected to be empty.
     pub fn empty(allocator: mem.Allocator, io: Io, file: Io.File) !Self {
@@ -221,13 +229,22 @@ pub const Tree = struct {
             .io = io,
             .file = file,
             .page_cache = .new(allocator, file, io),
+            .allocator = allocator,
         };
     }
 
-    pub fn find(self: *Self, key: []const u8) !FindResult {
+    pub fn _find(self: *Self, key: []const u8, options: FindOptions) !FindResult {
+        var path: ?std.ArrayList(PageId) = if (options.track_path)
+            .empty
+        else
+            null;
+
         var page_id: u64 = self.root_page_id;
 
         while (true) {
+            if (options.track_path)
+                try path.?.append(self.allocator, page_id);
+
             var next_page_id: ?PageId = null;
             const page = (try self.page_cache.get(page_id)).?;
             const header = page.header();
@@ -236,7 +253,7 @@ pub const Tree = struct {
             var high: usize = page.pointers().len;
 
             if (high == 0)
-                return .{ .cell = null, .page = page, .upper_bound_idx = null };
+                return .{ .cell = null, .page = page, .upper_bound_idx = null, .path = path };
 
             while (low < high) {
                 const mid = low + (high - low) / 2;
@@ -256,7 +273,7 @@ pub const Tree = struct {
                                 break;
                             },
                             .leaf => {
-                                return .{ .cell = cell, .page = page, .upper_bound_idx = upper_bound_idx };
+                                return .{ .cell = cell, .page = page, .upper_bound_idx = upper_bound_idx, .path = path };
                             },
                         }
                     },
@@ -274,6 +291,7 @@ pub const Tree = struct {
                     .cell = null,
                     .page = page,
                     .upper_bound_idx = upper_bound_idx,
+                    .path = path,
                 };
             }
 
@@ -291,27 +309,60 @@ pub const Tree = struct {
         @panic("unreachable");
     }
 
-    pub fn insert(
-        self: *Self,
-        key: []const u8,
-        value: []const u8,
-    ) !void {
-        var find_result = try self.find(key);
-        var page: *PageBuffer = find_result.page;
-        defer self.page_cache.put(page);
+    pub fn find(self: *Self, key: []const u8) !FindResult {
+        return self._find(key, .{
+            .track_path = false,
+        });
+    }
 
-        if (find_result.cell != null)
-            return error.KeyAlreadyExists;
+    fn insert_separator(
+        self: *Self,
+        parent_page_id: PageId,
+        old_page_id: PageId,
+        new_page_id: PageId,
+        key: []const u8,
+        path: *std.ArrayList(PageId),
+    ) !void {
+        _ = path;
+
+        const _value = old_page_id;
+        const value: []const u8 = @ptrCast(&_value);
+        const page = (try self.page_cache.get(parent_page_id)).?;
 
         // now, figure out where to insert
         const expected_size = key.len + value.len + @sizeOf(u64) * 2;
-        if (find_result.page.header().freeSpace() < expected_size + @sizeOf(CellOffset)) {
-            @panic("split unimplemented");
+        if (page.header().freeSpace() < expected_size + @sizeOf(CellOffset)) {
+            @panic("propagting split not supported");
         }
-        const insert_idx = find_result.upper_bound_idx orelse page.pointers().len;
-        if (find_result.upper_bound_idx) |idx| {
-            // shift pointers to right (starting from upper bound)
-            @memmove(page.pointers().ptr[0 .. page.pointers().len + 1][idx + 1 ..], page.pointers()[idx..]);
+
+        var upper_bound_idx: ?usize = null;
+        var low: usize = 0;
+        var high: usize = page.pointers().len;
+        if (high == 0)
+            upper_bound_idx = 0;
+
+        while (low < high) {
+            const mid = low + (high - low) / 2;
+            const midOffset: CellOffset = page.pointers()[mid];
+            var cell: Cell = page.cell(midOffset);
+            switch (mem.order(u8, key, cell.key())) {
+                .eq => {
+                    @panic("unreachable - parent already has the key");
+                },
+                .lt => {
+                    high = mid;
+                    upper_bound_idx = mid;
+                },
+                .gt => low = mid + 1,
+            }
+        }
+
+        // TODO: pointers.len is wrong when we're a key into a parent, and not in a found page directly.
+        //      we should find upper bound for parent (current) page too.
+        // shift pointers to right (starting from upper bound)
+        const insert_idx = upper_bound_idx orelse page.pointers().len;
+        if (insert_idx != page.pointers().len) {
+            @memmove(page.pointers().ptr[0 .. page.pointers().len + 1][insert_idx + 1 ..], page.pointers()[insert_idx..]);
         }
         page.header().number_of_cells += 1;
         page.header().upper -= expected_size;
@@ -319,7 +370,112 @@ pub const Tree = struct {
         page.pointers()[insert_idx] = page.header().upper;
         var cell: Cell = .raw(page.inner[page.header().upper..].ptr);
         cell.from_keyval(key, value);
+        if (insert_idx + 1 == page.pointers().len) {
+            page.header().right_pointer = new_page_id;
+        } else {
+            const p: []const u8 = @ptrCast(&new_page_id);
+            var after_cell = page.cell(page.offset(insert_idx + 1));
+            @memcpy(after_cell.val(), p);
+        }
         self.page_cache.mark_page_dirty(page);
+    }
+
+    /// Split `page` into two pages.
+    /// The existing `page` is the left half, and the returned page
+    /// is the right half.
+    ///
+    /// The caller is responsible for updating the parent pointers.
+    /// 
+    /// left: [0..split_idx)
+    /// right: [split_idx..]
+    ///
+    /// [1, 3, 4, 5, 6, 7]
+    /// 
+    /// left: [1, 3, 4]
+    /// right: [5, 6, 7]
+    pub fn split_page(self: *Self, page: *PageBuffer) !*PageBuffer {
+        const new_page_id = try self.page_cache.allocate();
+        const new_page = try self.page_cache.get(new_page_id) orelse @panic("unreachable");
+        new_page.header().* = .empty(page.header().type);
+        const split_index = page.pointers().len / 2;
+        // fill new page
+        for (page.pointers()[split_index..], 0..) |offset, idx| {
+            new_page.header().number_of_cells += 1;
+            new_page.header().lower += @sizeOf(CellOffset);
+            new_page.pointers()[idx] = page.header().upper;
+
+            var old_cell = page.cell(offset);
+            var new_cell = new_page.cell(new_page.header().upper);
+            new_page.header().upper -= old_cell.len();
+            new_cell.from_keyval(old_cell.key(), old_cell.val());
+
+            page.header().number_of_cells -= 1;
+            page.header().lower -= @sizeOf(CellOffset);
+            page.header().upper += new_cell.len();
+        }
+
+        return new_page;
+    }
+
+    pub fn insert(
+        self: *Self,
+        key: []const u8,
+        value: []const u8,
+    ) !void {
+        var find_result = try self._find(key, .{ .track_path = true });
+        var page: *PageBuffer = find_result.page;
+        defer self.page_cache.put(page);
+        defer find_result.path.?.deinit(self.allocator);
+
+        if (find_result.cell != null)
+            return error.KeyAlreadyExists;
+
+        // TODO: page and index to insert in, could change if we split
+        var target_page: *PageBuffer = page;
+        var insert_idx = find_result.upper_bound_idx orelse page.pointers().len;
+
+        // now, figure out where to insert
+        const expected_size = key.len + value.len + @sizeOf(u64) * 2;
+        if (find_result.page.header().freeSpace() < expected_size + @sizeOf(CellOffset)) {
+            var path = find_result.path orelse @panic("path is tracked");
+            const new_page = try self.split_page(page);
+            // TODO: same calculation is in split_page, keep in sync
+            const split_index = page.pointers().len / 2;
+            if (insert_idx >= split_index) {
+                target_page = new_page;
+                insert_idx -= split_index;
+            }
+            _ = path.pop(); // old leaf id
+            const parent_id = parent_id: {
+                if (path.pop()) |id| break :parent_id id;
+                const new_root_id = try self.page_cache.allocate();
+                var new_root = (try self.page_cache.get(new_root_id)).?;
+                new_root.header().* = .empty(.internal);
+                break :parent_id new_root_id;
+            };
+            try self.insert_separator(
+                parent_id,
+                page.page_id,
+                new_page.page_id,
+                key,
+                &path,
+            );
+        }
+
+        if (find_result.upper_bound_idx) |idx| {
+            // shift pointers to right (starting from upper bound)
+            @memmove(
+                target_page.pointers().ptr[0 .. target_page.pointers().len + 1][idx + 1 ..],
+                target_page.pointers()[idx..],
+            );
+        }
+        target_page.header().number_of_cells += 1;
+        target_page.header().upper -= expected_size;
+        target_page.header().lower += @sizeOf(CellOffset);
+        target_page.pointers()[insert_idx] = target_page.header().upper;
+        var cell: Cell = .raw(target_page.inner[target_page.header().upper..].ptr);
+        cell.from_keyval(key, value);
+        self.page_cache.mark_page_dirty(target_page);
     }
 
     pub fn deinit(self: *Self) void {
